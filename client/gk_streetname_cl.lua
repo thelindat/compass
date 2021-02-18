dc60f7644068f6f3a15852ea3ec6a64a0109f85c
thelindat <65407488+thelindat@users.noreply.github.com>
2021-02-18 13:43:33 +1100

    Update gk_streetname_cl.lua
    Replaced InvokeNative
    Use PlayerPedId() instead of GetPlayerPed
    Remove drawText and use NUI instead
    No longer draw/update every frame

--- client/gk_streetname_cl.lua
+++ client/gk_streetname_cl.lua
@@ -1,4 +1,3 @@
-
 Citizen.CreateThread( function()
 	local lastStreetA = 0
 	local lastStreetB = 0
@@ -6,9 +5,9 @@ Citizen.CreateThread( function()
 	while Config.streetName.show do
 		Citizen.Wait(0)
 
-		local playerPos = GetEntityCoords(GetPlayerPed(-1), true)
-		local streetA, streetB = Citizen.InvokeNative(0x2EB41072B4C1E4C0, playerPos.x, playerPos.y, playerPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt())
-		local street = {}
+		local playerPos = GetEntityCoords(PlayerPedId(), true)
+		local streetA, streetB = GetStreetNameAtCoord(playerPos.x, playerPos.y, playerPos.z)
+		street = {}
 
 		if not ((streetA == lastStreetA or streetA == lastStreetB) and (streetB == lastStreetA or streetB == lastStreetB)) then
 			lastStreetA = streetA
@@ -23,13 +22,13 @@ Citizen.CreateThread( function()
 			table.insert(street, GetStreetNameFromHashKey(lastStreetB))
 		end
 
-		drawText(table.concat(street, " & "), Config.streetName.position.x, Config.streetName.position.y, {
-			size = Config.streetName.textSize,
-			colour = Config.streetName.textColour,
-			outline = true,
-			centered = Config.streetName.position.centered,
-			font = 1,
-			size = 0.55
-		})
+		street = table.concat(street, " & "), Config.streetName.position.x, Config.streetName.position.y
+
+		if street ~= laststreet then
+			SendNUIMessage({action = "display", type = street})
+			Citizen.Wait(100)
+		end
+		laststreet = street
+		Citizen.Wait(100)
 	end
 end)
